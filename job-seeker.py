2d5e64af5eed67f7d15f1b56ec29147dd68c2077
javisalva9 <javisalva9@gmail.com>
2025-03-02 22:29:36 +0100

    remote and salary

--- job-seeker.py
+++ job-seeker.py
@@ -2,18 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 
 def scrape_jobfluent():
-    url = 'https://www.jobfluent.com/jobs-barcelona'
+    url = 'https://www.jobfluent.com/es/empleos-remoto'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
     jobs = []
     for job_card in soup.find_all('div', class_='panel-offer'):
-        print(job_card)
         title = job_card.find('h3').a.text.strip()
         salary_tag = job_card.find('span', class_='salary')
         salary = salary_tag.text.strip() if salary_tag else 'N/A'
         # location = job_card.find('span', class_='location').text.strip()
-        link = job_card.find('a')['href']
+        link = 'https://www.jobfluent.com/es/empleos' + job_card.find('a')['href']
         
         jobs.append({
             'title': title,
@@ -29,7 +28,7 @@ if __name__ == '__main__':
     for job in job_listings:
         print(f"Title: {job['title']}")
         # print(f"Company: {job['company']}")
-        # print(f"Location: {job['location']}")
+        print(f"Salary: {job['salary']}")
         print(f"Link: {job['link']}")
         print('-' * 20)
     print(f"Soy concha, me voy a dormir")
