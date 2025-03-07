2cc50781a53990a7276b6313b1f6a4e707ac8c2a
javisalva <javisalva9@gmail.com>
2025-03-07 16:56:52 +0100

    mi spreshit

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ Miguel = User(name="Miguel", search_query="nodejs", spreadsheet_id="1tRxmT2ZTvL5
 I am a Node.js backend developer with over 5 years of experience creating scalable APIs in Node, TypeScript, and Nest.js. My expertise includes architecting subscription payment solutions, designing custom CMS tools for rapid deployments, migrating critical logic from frontend to backend, and optimizing error logging with AWS-based monitoring. I also have a proven track record working with MongoDB, Jenkins, and remote teams aligned with Spanish business hours. While I thrive in collaborative environments, I prefer to focus on backend development rather than full-stack or Java-intensive roles.
 """
 )
-Javi = User(name="Javi", search_query="python", spreadsheet_id="YOUR_SPREADSHEET_ID", work_preferences= "I want to work with cats")
+Javi = User(name="Javi", search_query="python", spreadsheet_id="1VmlhE-AThc2QAaTTgqRMSxm87fXs4a_ysa0ihveK1V0", work_preferences= "I want to work with cats")
 
 class AIQueries:
     MATCH_AND_RATE = """1. Input to AI: “Here is the developer profile above. Below is a job description. Compare the job description’s requirements with the developer’s experience. Rate the match on a scale from 0 to 100 and provide a concise explanation for why you assigned that score.”
